c4bf3f2f7e6ad28db5bbaa3ca9dc3bd1ff77308c
paulstoner <iampaulinus7@gmail.com>
2024-08-21 14:07:31 +0100

    update

--- src/pages/Hoodie/ContentHoodie.jsx
+++ src/pages/Hoodie/ContentHoodie.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './hoodies.css'
 import { FaSearch, FaStar} from 'react-icons/fa'
 import 'remixicon/fonts/remixicon.css';
-import  { useState } from 'react';
+
+
+
+
+
 
 
 
@@ -34,12 +38,7 @@ const HoodieSect = (props) =>{
 
 
 const ContentHoodie = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxChange = (event) => {
-    setIsChecked(event.target.checked);
-  };
-
+  
 
 
   return (
@@ -52,57 +51,75 @@ const ContentHoodie = () => {
       <div className="container containerr">
       <div className="scrollable left">
         <div className="content">
-          <div className='search-container'>
-            <input id='prod-search' type="text" placeholder='Search Products' />
-            <a href="/"><FaSearch className='search-icon'/></a>
+          <div className='search-cont'>
+            <input id='prod-search' type="text" placeholder='Search products...' />
+            <a href=""><FaSearch className='search-icon'/></a>
           </div>
-          <div>
-            <h5>Product Categories</h5>
-            <p>Male</p>
-            <p>Female</p>
-            <p>Boy</p>
-            <p>Girl</p>
-            <p>Unisex</p>
-            <p>Children</p>
+          <div className='left-dvcont'>
+            <h6>Product Categories</h6>
+            <a href=""><p>Male</p></a>
+            <a href=""><p>Female</p></a>
+            <a href=""><p>Boy</p></a>
+            <a href=""><p>Girl</p></a>
+            <a href=""><p>Unisex</p></a>
+            <a href=""><p>Children</p></a>
           </div>
 
-          <div>
-            <h5>Size</h5>
-            <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                XS
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                S
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                M
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                L
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                XL
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                XXL
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
-              <label>
-              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
-                XXXL
-            </label>
-              {isChecked && <div>This is the content that is shown when the checkbox is checked.</div>}
+          <div className='left-dvcont'>
+            <h6>Size</h6>
+            <div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>XS</p>
+                  <p>15</p>
+                  </a>
+                
+              </div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>S</p>
+                  <p>10</p>
+                  </a>
+                
+              </div>
+              <div className='sz-dv'>
+               <a href="">
+                <p>M</p>
+                <p>25</p>
+                </a>
+                  
+              </div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>L</p>
+                  <p>17</p>
+                  </a>
+                
+              </div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>XL</p>
+                  <p>17</p>
+                  </a>
+                
+              </div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>XXL</p>
+                  <p>10</p>
+                  </a>
+                
+              </div>
+              <div className='sz-dv'>
+                <a href="">
+                  <p>XXXL</p>
+                  <p>27</p>
+                  </a>
+               
+              </div>
+              
+            </div>
+          
           </div>
         
           
@@ -123,7 +140,7 @@ const ContentHoodie = () => {
                     bottun= "Add to cart"/> 
         </div>
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723027607/Link_16-2-330x440.jpg_vgixgr.png"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
@@ -132,7 +149,7 @@ const ContentHoodie = () => {
                     bottun= "Add to cart"/> 
         </div>
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723028442/Sweat_%C3%A0_capuche_vert_%C3%A0_manches_longues_avec_poche_kangourou_pour_homme_-_Vert___XL_zh8si3.jpg"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
@@ -141,7 +158,7 @@ const ContentHoodie = () => {
                     bottun= "Add to cart"/> 
         </div>
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723028442/Men_Letter_Graphic_Kangaroo_Pocket_Drawstring_Thermal_Hoodie_edt5vz.jpg"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
@@ -157,7 +174,7 @@ const ContentHoodie = () => {
 
     <div className="right2">
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723028441/Oversize_Sports_Car_F40_Print_Hoodie_gdqozr.jpg"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
@@ -166,7 +183,7 @@ const ContentHoodie = () => {
                     bottun= "Add to cart"/> 
         </div>
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723028441/FASHION_Casual_Hoodie_-_Green_Diag___US_XS_Asian_M_wk0dgp.jpg"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
@@ -175,7 +192,7 @@ const ContentHoodie = () => {
                     bottun= "Add to cart"/> 
         </div>
         <div className='hoddy'>
-          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1721287852/istockphoto-1336885284-612x612_xampqp.jpg"
+          <HoodieSect imge= "https://res.cloudinary.com/ds2swdt9n/image/upload/v1723028441/Hustlers_hoodie_Make_Money_Not_Friends_Fleece_Oversized_relax_fit_hoodie_-_Navy_Blue___L_siidec.jpg"
                     fav= ""
                     discrp= "Elegant Apparel quality plain hoodie for boys and girls"
                     amt= "15,000"
